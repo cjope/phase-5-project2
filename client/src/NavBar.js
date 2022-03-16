92a3465d02a8e96470c4bc1d9fec744e1d5b79da
Craig <craigjope@gmail.com>
2022-03-16 16:03:02 -0600

    Navbar Cleanup

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,8 +1,33 @@
-function NavBar() {
+import Login from "./Login";
+import Logout from "./Logout";
+import SignUp from "./SignUp";
+
+function NavBar({ user, setUser }) {
   return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
+    <>
+        <div className="nav">
+            <h1>Pantry Pal</h1>
+        <div >
+            {user ? 
+              <div className="nav">
+                <p>Welcome, {user.first_name}!</p>
+                  <img
+                    className="profile-pic"
+                    src={user.image.url}
+                    alt="profile"
+                  />
+                <Logout setUser={setUser} />
+              </div>
+             : 
+              <div >
+                <Login setUser={setUser}></Login>
+                <SignUp setUser={setUser}></SignUp>
+              </div>
+            }
+            </div>
+            </div>
+    </>
   );
 }
+
 export default NavBar;
